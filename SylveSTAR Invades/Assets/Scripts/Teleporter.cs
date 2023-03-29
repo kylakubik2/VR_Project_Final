dbbeb41caac49fcae55d7b9b7eb3a17cb3ee06f0
shiindeeruu <89371031+shiindeeruu@users.noreply.github.com>
2023-03-29 15:51:25 -0500

    added snap location to gun and updated golf2 location

--- SylveSTAR Invades/Assets/Scripts/Teleporter.cs
+++ SylveSTAR Invades/Assets/Scripts/Teleporter.cs
@@ -8,7 +8,7 @@ public class Teleporter : MonoBehaviour
     public Vector3 startPosition = new Vector3(0.0f, 3.0f, 0.0f);
     public Vector3 racingPosition = new Vector3(-595.58f, 21.11f, 247.71f); 
     public Vector3 golf1Position = new Vector3(-401.0f, 0.0f, 296.0f);
-    public Vector3 golf2Position = new Vector3(-444.0f, 0.0f, 446.0f);
+    public Vector3 golf2Position = new Vector3(-392.0f, 747.0f, 490.89f);
     public Vector3 golf3Position = new Vector3(-408.0f, 0.0f, 728.0f);
 
     public GameObject player;
@@ -65,24 +65,8 @@ public class Teleporter : MonoBehaviour
             strokesText.enabled = true;
             parText.enabled = true;
 
-
-            if (room == 1)
-            {
-                player.transform.position = golf1Position;
-                parText.text = "Par: ";
-            }
-            else if (room == 2)
-            {
-                Debug.Log("got to part 2");
-                player.transform.position = golf2Position;
-                parText.text = "Par: ";
-            }
-            else
-            {
-                Debug.Log("got to part 3");
-                player.transform.position = golf3Position;
-                parText.text = "Par: ";
-            }
+            player.transform.position = golf2Position;
+            parText.text = "Par: ";
         }
     }
 }
